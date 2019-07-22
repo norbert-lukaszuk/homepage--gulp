7b0b242e6c65380d2a06f96a4fe27230ebb163c6
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-22 16:26:14 +0200

    transfering to gulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,7 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+const menuButton = document.querySelector('.menu__button--js')
+menuButton.addEventListener('click',(e)=>{
+    const navigationList = document.querySelector('.navigationList--js');
+    navigationList.classList.toggle('navigationList--visible');
+   
+    
+});

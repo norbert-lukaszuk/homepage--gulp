9b334d26dc9cf4ae363ea2545675ac1b1a3e228c
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-23 10:51:25 +0200

    transfering to galp

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const menuButton = document.querySelector('.menu__button--js')
+L
 menuButton.addEventListener('click',(e)=>{
     const navigationList = document.querySelector('.navigationList--js');
     navigationList.classList.toggle('navigationList--visible');

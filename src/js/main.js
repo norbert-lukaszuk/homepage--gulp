6ff30054a65fce771d847fbe3db2d2a77af806dc
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-23 16:51:17 +0200

    transfering to gulp

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 const menuButton = document.querySelector('.menu__button--js')
-L
+
 menuButton.addEventListener('click',(e)=>{
     const navigationList = document.querySelector('.navigationList--js');
     navigationList.classList.toggle('navigationList--visible');
-   
+  
+    
     
 });

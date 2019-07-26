e38179190b6e68a6505256716d2395ed481836ea
Norbert Łukaszuk <norbert.lukaszuk@gmail.com>
2019-07-26 10:41:14 +0200

    mediaquery & menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 const menuButton = document.querySelector('.menu__button--js')
 
 menuButton.addEventListener('click',(e)=>{
-    const navigationList = document.querySelector('.navigationList--js');
-    navigationList.classList.toggle('navigationList--visible');
-  
-    
+    // const navigationList = document.querySelector('.navigationList--js');
+    // navigationList.classList.toggle('navigationList--visible'); 
+    const mainNavigation__list= document.querySelector('.mainNavigation__list');
+    mainNavigation__list.classList.toggle('mainNavigation__list--visible');    
+       
     
 });
+
